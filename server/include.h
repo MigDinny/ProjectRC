7721f43112b15dff12fcd622d91f319f8c5fcb5d
Edgar Duarte <edgartip2003@gmail.com>
2021-05-02 00:45:12 +0100

    Implements the TCP server

--- server/include.h
+++ server/include.h
@@ -5,15 +5,28 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+
 
 #define BUFLEN 512	// buffer length
 #define TCP_PORT 9000	// TCP port to admin CLI
-#define UDP_PORT 5000   // UDP port 
-#define MAX_USERS 20    // max users 
+#define UDP_PORT 5000   // UDP port
+#define MAX_USERS 20    // max users
+#define MAX_ARGUMENTS 7  //max number of arguments given to the admin CLI
+#define ARGUMENT_LEN 20  //length of an argument given to the admin CLI
+#define REPLY_SIZE 256
 
 typedef struct user {
     char username[20];
+    char ip[20];
     char password[20];
+    char client_server[4];
+    char p2p[4];
+    char group[4];
 
 } user;
 
@@ -21,6 +34,27 @@ user userlist[MAX_USERS];
 
 void erro(char *);
 
-void TCPWorker();
+void *TCPWorker(void*);
+
+void listUsers();
+
+int valueVerification(char arguments[MAX_ARGUMENTS][ARGUMENT_LEN]);
+
+void addUser(char arguments[MAX_ARGUMENTS][ARGUMENT_LEN]);
+
+void deleteUser(char arguments[MAX_ARGUMENTS][ARGUMENT_LEN]);
 
 void sigint(int);
+
+void init();
+
+void writeToFile();
+
+pthread_t TCPThreadID;
+
+int tcp_fd, tcp_client_fd;
+struct sockaddr_in tcp_addr, tcp_client_addr;
+int tcp_client_addr_size;
+
+FILE* fp;
+char *file_name = "ficheiro.txt";
